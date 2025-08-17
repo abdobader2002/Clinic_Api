47592379b186cadd065523a457d4a0ccdf3a5a91
abdobadr2002 <abdelrahman2000835@gmail.com>
2025-08-17 10:50:25 +0300

    Refactor authentication with DTOs and new methods
    - Removed DTO folder from `Clinic_Api.csproj`.
    - Updated `AuthController.cs` to include DTOs and added `Register` and `Login` methods for user authentication.
    - Created `LoginDto.cs` for login data validation.
    - Created `RegisterDto.cs` for registration data validation.

--- Clinic_Api/Controllers/AuthController.cs
+++ Clinic_Api/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
-﻿using Clinic_Api.Models;
+﻿using Clinic_Api.DTOs;
+using Clinic_Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clinic_Api.Controllers
 {
@@ -14,6 +16,77 @@ namespace Clinic_Api.Controllers
         {
             _context = context;
         }
-        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var existingUser = await _context.Patients
+               .FirstOrDefaultAsync(p => p.Email == registerDto.Email);
+
+            if (existingUser != null)
+            {
+                return Conflict(new
+                {
+                    Success = false,
+                    Message = "User with this email already exists"
+                });
+            }
+            var patient = new Patient
+            {
+                Name = registerDto.Name,
+                Email = registerDto.Email,
+                Password = registerDto.Password,
+                Phone = registerDto.Phone,
+                Gender = registerDto.Gender,
+            };
+            try
+            {
+                _context.Users.Add(patient);
+                await _context.SaveChangesAsync();
+                return Ok(new {
+                    Success= true,
+                    Message = "User registered successfully",
+                    Data = new
+                    {
+                        Id = patient.Id,
+                        Name = patient.Name,
+                        Email = patient.Email,
+                        Phone = patient.Phone,
+                        Gender = patient.Gender,
+                        Role = "Patient",
+                        Birthdate = patient.Birthdate,
+                        MedicalRecord = patient.MedicalRecord,
+                        Bookings = new List<object>() 
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+               
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while registering the user");
+            }
+
+        } 
+        //login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == loginDto.Email && u.Password == loginDto.Password);
+            if (user == null)
+            {
+                return Unauthorized(new { Success = false, Message = "Invalid email or password" });
+            }
+            return Ok(new { Success = true, Message = "Login successful", data = user });
+        }
+
     }
+
 }
